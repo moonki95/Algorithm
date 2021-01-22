6024647a52ebb8ab5422d912dd2b5da21e92a2e6
Moonki Jang <mkjang0905@naver.com>
2021-01-22 17:53:09 +0900

    A+B-6

---
+++ Algo study/10953.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int tc;
+	cin >> tc;
+
+	for (int i = 0; i < tc; i++) {
+		int a, b;
+		char str;
+		cin >> a >> str >> b;
+		cout << a + b << endl;
+	}
+	return 0;
+}
