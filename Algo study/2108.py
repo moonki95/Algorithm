b70da38a466e73ed895ffcad16e5f0a5dbcd68fc
Moonki Jang <mkjang0905@naver.com>
2021-05-28 23:58:34 +0900

    통계학

---
+++ Algo study/2108.py
@@ -0,0 +1,22 @@
+import sys
+from collections import Counter
+
+N = int(sys.stdin.readline())
+nums = [int(sys.stdin.readline()) for _ in range(N)]
+
+nums.sort()
+
+top_num = Counter(nums).most_common()
+
+print("%.f" % (sum(nums) / N))
+print(nums[N // 2])
+
+if len(nums) > 1:
+    if top_num[0][1] == top_num[1][1]:
+        print(top_num[1][0])
+    else:
+        print(top_num[0][0])
+else:
+    print(nums[0])
+
+print(nums[-1] - nums[0])
