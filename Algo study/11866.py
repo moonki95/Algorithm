6f9ad22a1bee442956779f5f9e5077dadb2c830b
Moonki Jang <mkjang0905@naver.com>
2021-06-07 14:42:19 +0900

    요세푸스문제 0

---
+++ Algo study/11866.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+n,k = map(int,input().split())
+
+queue = deque()
+ans = []
+for i in range(1,n+1):
+    queue.append(i)
+print('<',end='')
+while queue:
+    for i in range(k-1):
+        queue.append(queue[0])
+        queue.popleft()
+    print(queue.popleft(),end='')
+    if queue:
+        print(', ',end='')
+print('>')
